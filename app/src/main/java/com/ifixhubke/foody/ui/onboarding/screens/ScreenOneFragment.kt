821fb326f4ac4d42f7574c86de2cec398efb2342
Jackline-ke <antoninahodhiambo@gmail.com>
2021-08-17 16:13:48 +0300

    Screen1

--- app/src/main/java/com/ifixhubke/foody/ui/onboarding/screens/ScreenOneFragment.kt
+++ app/src/main/java/com/ifixhubke/foody/ui/onboarding/screens/ScreenOneFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.viewpager2.widget.ViewPager2
 import com.ifixhubke.foody.R
 
 class ScreenOneFragment : Fragment() {
@@ -14,11 +13,7 @@ class ScreenOneFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        val view = inflater.inflate(R.layout.fragment_screen_one, container, false)
-
-        val viewPager = view.findViewById<ViewPager2>(R.id.viewPager)
-
-        return view
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_screen_one, container, false)
     }
 }
