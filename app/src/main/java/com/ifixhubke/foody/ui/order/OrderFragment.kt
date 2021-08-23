38e29dddd52990a9c7126d5d8d073dee4da60645
Owallah <51329456+Owallah@users.noreply.github.com>
2021-08-23 14:52:55 +0300

    Worked on checking internet

--- app/src/main/java/com/ifixhubke/foody/ui/order/OrderFragment.kt
+++ app/src/main/java/com/ifixhubke/foody/ui/order/OrderFragment.kt
@@ -1,11 +1,11 @@
 package com.ifixhubke.foody.ui.order
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.fragment.app.Fragment
 import com.ifixhubke.foody.R
 
 class OrderFragment : Fragment() {
