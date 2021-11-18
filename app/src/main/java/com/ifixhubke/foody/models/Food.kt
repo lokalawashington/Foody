923ece1632622f38dbd72aaa5f12b52ccbc6e249
Collince-Okeyo <collinceokeyo98@gmail.com>
2021-11-18 17:26:42 +0300

    Models done

---
+++ app/src/main/java/com/ifixhubke/foody/models/Food.kt
@@ -0,0 +1,8 @@
+package com.ifixhubke.foody.models
+
+data class Food(
+    val id: Int?,
+    val foodImage: String?,
+    val foodName: String?,
+    val foodPrice: Int?,
+)
---
+++ app/src/main/java/com/ifixhubke/foody/models/Order.kt
@@ -0,0 +1,7 @@
+package com.ifixhubke.foody.models
+
+data class Order(
+    val numberOfOrders: Int?,
+    val tableNumber: String?,
+    val orderAmount: Int?
+)
